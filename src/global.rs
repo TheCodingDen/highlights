7a7441774a9b69ddbafb6af8d4492fb09eee3463
josh <joshyrobot@gmail.com>
2020-12-29 00:31:14 -0700

    add env config for max keywords

--- src/global.rs
+++ src/global.rs
@@ -6,8 +6,6 @@ use serenity::model::id::UserId;
 
 use std::{env, time::Duration};
 
-pub const MAX_KEYWORDS: u32 = 100;
-
 pub const NOTIFICATION_RETRIES: u8 = 5;
 
 pub const EMBED_COLOR: u32 = 0xefff47;
@@ -36,6 +34,9 @@ pub fn init_mentions(bot_id: UserId) {
 
 static PRIVATE_MODE: OnceCell<bool> = OnceCell::new();
 
+const DEFAULT_MAX_KEYWORDS: u32 = 100;
+static MAX_KEYWORDS: OnceCell<u32> = OnceCell::new();
+
 const DEFAULT_PATIENCE_DURATION: Duration = Duration::from_secs(60 * 2);
 static PATIENCE_DURATION: OnceCell<Duration> = OnceCell::new();
 
@@ -43,6 +44,12 @@ pub fn private_mode() -> bool {
 	*PRIVATE_MODE
 		.get()
 		.expect("Private mode env was not initialized")
+}
+
+pub fn max_keywords() -> u32 {
+	*MAX_KEYWORDS
+		.get()
+		.expect("Max keywords env was not initialized")
 }
 
 pub fn patience_duration() -> Duration {
@@ -53,6 +60,27 @@ pub fn patience_duration() -> Duration {
 
 pub fn init_env() {
 	let _ = PRIVATE_MODE.set(env::var_os("HIGHLIGHTS_PRIVATE").is_some());
+
+	let max_keywords = match env::var("HIGHLIGHTS_MAX_KEYWORDS") {
+		Ok(max) => match max.parse() {
+			Ok(max) => Some(max),
+			Err(e) => {
+				log::error!(
+					"HIGHLIGHTS_MAX_KEYWORDS is an invalid number ({}): {}",
+					max,
+					e
+				);
+				None
+			}
+		},
+		Err(env::VarError::NotUnicode(_)) => {
+			log::error!("HIGHLIGHTS_MAX_KEYWORDS is invalid UTF-8");
+			None
+		}
+		Err(env::VarError::NotPresent) => None,
+	}
+	.unwrap_or(DEFAULT_MAX_KEYWORDS);
+	let _ = MAX_KEYWORDS.set(max_keywords);
 
 	let patience_duration = match env::var("HIGHLIGHTS_PATIENCE_SECONDS") {
 		Ok(seconds) => match seconds.parse() {
