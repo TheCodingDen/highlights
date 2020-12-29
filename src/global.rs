78f5dc52d30c2fdebb0506215a50f1931ec66d14
josh <joshyrobot@gmail.com>
2020-12-29 00:25:31 -0700

    add env config for patience

--- src/global.rs
+++ src/global.rs
@@ -8,8 +8,6 @@ use std::{env, time::Duration};
 
 pub const MAX_KEYWORDS: u32 = 100;
 
-pub const PATIENCE_DURATION: Duration = Duration::from_secs(60 * 2);
-
 pub const NOTIFICATION_RETRIES: u8 = 5;
 
 pub const EMBED_COLOR: u32 = 0xefff47;
@@ -38,12 +36,42 @@ pub fn init_mentions(bot_id: UserId) {
 
 static PRIVATE_MODE: OnceCell<bool> = OnceCell::new();
 
+const DEFAULT_PATIENCE_DURATION: Duration = Duration::from_secs(60 * 2);
+static PATIENCE_DURATION: OnceCell<Duration> = OnceCell::new();
+
 pub fn private_mode() -> bool {
 	*PRIVATE_MODE
 		.get()
 		.expect("Private mode env was not initialized")
 }
 
+pub fn patience_duration() -> Duration {
+	*PATIENCE_DURATION
+		.get()
+		.expect("Patience duration env was not initialized")
+}
+
 pub fn init_env() {
 	let _ = PRIVATE_MODE.set(env::var_os("HIGHLIGHTS_PRIVATE").is_some());
+
+	let patience_duration = match env::var("HIGHLIGHTS_PATIENCE_SECONDS") {
+		Ok(seconds) => match seconds.parse() {
+			Ok(seconds) => Some(seconds),
+			Err(e) => {
+				log::error!(
+					"HIGHLIGHTS_PATIENCE_SECONDS is an invalid number ({}): {}",
+					seconds,
+					e
+				);
+				None
+			}
+		},
+		Err(env::VarError::NotUnicode(_)) => {
+			log::error!("HIGHLIGHTS_PATIENCE_SECONDS is invalid UTF-8");
+			None
+		}
+		Err(env::VarError::NotPresent) => None,
+	}
+	.map_or(DEFAULT_PATIENCE_DURATION, Duration::from_secs);
+	let _ = PATIENCE_DURATION.set(patience_duration);
 }
